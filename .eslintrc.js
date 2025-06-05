7fd4e656741313794d8db01ac4272366c496d47f
rajeshmn47 <rajeshmn47@gmail.com>
2025-06-05 20:03:03 +0530

    fix

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+{/*module.exports = {
+env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["airbnb"],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["simple-import-sort","unused-imports"],
+  rules: {},
+  rules: {
+    // increase the severity of rules so they are auto-fixable
+    "simple-import-sort/imports": "error",
+    "unused-imports/no-unused-imports": "error"
+  },
+}
+*/}
